e1e127d9daf8fcb10fbad9274606793fd4609a6f
Artsiom Miklushou <mikla.op@gmail.com>
2016-04-26 00:12:08 +0300

    Finite-state machines. Changing states.

--- finite-state-machine/src/main/scala/Main.scala
+++ finite-state-machine/src/main/scala/Main.scala
@@ -1,24 +1,54 @@
-import akka.actor.{FSM, Actor}
+import akka.actor.FSM.Event
+import akka.actor._
+import scala.concurrent.ExecutionContext.global
 
 object Main extends App {
-	
+
+  val system = ActorSystem("fsm-actors")
+  val inventory = system.actorOf(Props[Inventory])
+
+  inventory ! StateRequest()
+  inventory ! StateTwo
+  inventory ! StateRequest()
+
+  readLine()
+
+  system.shutdown()
 }
 
-class Inventory() extends Actor with FSM[State, StateData] {
+class Inventory()
+  extends Actor
+    with FSM[State, StateData]
+    with ActorLogging {
+
   startWith(StateOne, StateData("state1-data"))
 
   when(StateOne) {
     case Event(request: StateRequest, data: StateData) =>
+      println("Requesting state", data)
       stay
     case Event(StateTwo, data: StateData) =>
-      goto(StateTwo) using data // provide new state data
+      println("Changing state", data)
+      goto(StateTwo) using data.copy(data = "state2-data") // provide new state data
 
   }
+  when(StateTwo) {
+    case Event(_, data) =>
+      println("StateTwo got", data)
+      stay
+  }
+  onTransition {
+    case StateOne -> StateTwo =>
+      println("onTransition, changing state")
+  }
+
   whenUnhandled {
     case Event(e, s) =>
       println("unhandled state {} {}", e, s)
       stay
   }
+
+  initialize()
 }
 
 sealed trait State
